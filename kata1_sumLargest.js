66d7b4a9ee50bdc10cf2115a4586158e72e82928
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 19:22:41 +0000

    Completed revisit of aum largest (kata 1)

---
+++ kata1_sumLargest.js
@@ -0,0 +1,19 @@
+const sumLargestNumbers = function(data) {
+  let largest = data[0];
+  let secondLargest = data[1];
+
+  data.forEach(datum => {
+    if (datum > largest) {
+      secondLargest = largest;
+      largest = datum;
+    } else if (datum > secondLargest) {
+      secondLargest = datum;
+    }
+  })
+
+  return largest + secondLargest;
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
