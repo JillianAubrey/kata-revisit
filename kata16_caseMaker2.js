11a640bf72fd11e49cb2174213da5f68346757ce
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 17:15:50 +0000

    Implemented upper and lower cases to case maker (kata 16)

--- kata16_caseMaker2.js
+++ kata16_caseMaker2.js
@@ -63,11 +63,27 @@ const vowel = text => {
   }).join('');
 };
 
+const consonant = text => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return text.toLowerCase().split('').map(c => {
+    if (!vowels.includes(c)) {
+      return c.toUpperCase();
+    }
+    return c;
+  }).join('');
+};
+
+const upper = text => text.toUpperCase();
+
+const lower = text => text.toLowerCase();
+
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
 console.log(makeCase("this is a string", "snake"));
 console.log(makeCase("this is a string", "kebab"));
 console.log(makeCase("this is a string", "title"));
 console.log(makeCase("this is a string", "vowel"));
-// console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", "consonant"));
+console.log(makeCase("this is a string", "upper"));
+console.log(makeCase("this is a string", "lower"));
 // console.log(makeCase("this is a string", ["upper", "snake"]));
