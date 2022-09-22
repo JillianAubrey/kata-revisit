f0371ded727acd3ffd81e261f270f5f5e3d0b161
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 17:07:12 +0000

    Implemented kebab case in case maker (kata 16)

--- kata16_caseMaker2.js
+++ kata16_caseMaker2.js
@@ -35,10 +35,19 @@ const snake = text => {
   }).join('');
 };
 
+const kebab = text => {
+  return text.split('').map(c => {
+    if (c === ' ') {
+      return '-';
+    }
+    return c;
+  }).join('');
+};
+
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
 console.log(makeCase("this is a string", "snake"));
-// console.log(makeCase("this is a string", "kebab"));
+console.log(makeCase("this is a string", "kebab"));
 // console.log(makeCase("this is a string", "title"));
 // console.log(makeCase("this is a string", "vowel"));
 // console.log(makeCase("this is a string", "consonant"));
