2702b7ea82874a3239b53c986259278f86f3d7cd
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 19:33:16 +0000

    Completed revisit of vowels (kata 3)

---
+++ kata3_vowels.js
@@ -0,0 +1,8 @@
+const numberOfVowels = function(data) {
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  return data.split('').reduce((counter, char) => (vowels.includes(char) ? counter + 1 : counter), 0);
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
