16b11defdb8af73bd16ecac6e5dff1b8f459d01b
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 17:01:37 +0000

    Implemented camel case in case maker (kata 16)

---
+++ kata16_caseMaker2.js
@@ -0,0 +1,24 @@
+const makeCase = function(input, caseOption) {
+  return eval(caseOption)(input);
+}
+
+const camel = text => {
+  return text.toLowerCase().split('').map((c, i, a) => {
+    if (c === ' ') {
+      return '';
+    }
+    if (a[i-1] === ' ') {
+      return c.toUpperCase();
+    }
+    return c;
+  }).join('');
+};
+
+console.log(makeCase("this is a string", "camel"));
+console.log(makeCase("this is a string", "pascal"));
+// console.log(makeCase("this is a string", "snake"));
+// console.log(makeCase("this is a string", "kebab"));
+// console.log(makeCase("this is a string", "title"));
+// console.log(makeCase("this is a string", "vowel"));
+// console.log(makeCase("this is a string", "consonant"));
+// console.log(makeCase("this is a string", ["upper", "snake"]));
