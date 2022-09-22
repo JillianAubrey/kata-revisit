2cc95c72f273190c0e04130551ad4bc3a05827ea
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 17:10:18 +0000

    Implemented title case in case maker (kata 16)

--- kata16_caseMaker2.js
+++ kata16_caseMaker2.js
@@ -44,11 +44,20 @@ const kebab = text => {
   }).join('');
 };
 
+const title = text => {
+  return text.toLowerCase().split('').map((c, i, a) => {
+    if (i === 0 || a[i-1] === ' ') {
+      return c.toUpperCase();
+    }
+    return c;
+  }).join('');
+};
+
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
 console.log(makeCase("this is a string", "snake"));
 console.log(makeCase("this is a string", "kebab"));
-// console.log(makeCase("this is a string", "title"));
+console.log(makeCase("this is a string", "title"));
 // console.log(makeCase("this is a string", "vowel"));
 // console.log(makeCase("this is a string", "consonant"));
 // console.log(makeCase("this is a string", ["upper", "snake"]));
