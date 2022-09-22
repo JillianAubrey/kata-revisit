100e03bec11cf9a6daf60d6ce9fc95cf7ece4083
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 17:11:11 +0000

    Implemented vowel case in case maker (kata 16)

--- kata16_caseMaker2.js
+++ kata16_caseMaker2.js
@@ -53,11 +53,21 @@ const title = text => {
   }).join('');
 };
 
+const vowel = text => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  return text.toLowerCase().split('').map(c => {
+    if (vowels.includes(c)) {
+      return c.toUpperCase();
+    }
+    return c;
+  }).join('');
+};
+
 console.log(makeCase("this is a string", "camel"));
 console.log(makeCase("this is a string", "pascal"));
 console.log(makeCase("this is a string", "snake"));
 console.log(makeCase("this is a string", "kebab"));
 console.log(makeCase("this is a string", "title"));
-// console.log(makeCase("this is a string", "vowel"));
+console.log(makeCase("this is a string", "vowel"));
 // console.log(makeCase("this is a string", "consonant"));
 // console.log(makeCase("this is a string", ["upper", "snake"]));
