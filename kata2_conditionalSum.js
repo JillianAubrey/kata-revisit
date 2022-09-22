8bb1dc4af3e7ca0ac0e222027b3e9f096252912f
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-22 19:29:53 +0000

    Completed revisit of conditional sum (kata 2)

---
+++ kata2_conditionalSum.js
@@ -0,0 +1,15 @@
+const conditionalSum = function(values, condition) {
+  const condSum = values.reduce((a, b) => {
+    if (( b % 2 === 0 ? condition === 'even' : condition === 'odd' )) {
+      return a + b;
+    }
+    return a
+  }, 0);
+
+  return condSum;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
