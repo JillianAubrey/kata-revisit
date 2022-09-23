ef114356ab8043bbcddb34b09f19565a01767da7
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-23 01:10:25 +0000

    Completed revisit of choose recipe (kata 12)

---
+++ kata12_chooseRecipe.js
@@ -0,0 +1,49 @@
+const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+  let chosenRecipe = '';
+  recipes.every(recipe => {
+    const recipeCheck = recipe.ingredients.reduce((check, ing) => check && (bakeryA.includes(ing) || bakeryB.includes(ing)), true);
+    if (recipeCheck) {
+      chosenRecipe = recipe.name;
+    }
+    return !recipeCheck;
+  });
+  return chosenRecipe;
+};
+
+let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
+let bakeryB = ['milk', 'butter', 'cream cheese'];
+let recipes = [
+  {
+    name: 'Coconut Sponge Cake',
+    ingredients: ['coconut', 'cake base']
+  },
+  {
+    name: 'Persian Cheesecake',
+    ingredients: ['saffron', 'cream cheese']
+  },
+  {
+    name: 'Custard Surprise',
+    ingredients: ['custard', 'ground beef']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+bakeryA = ['potatoes', 'bay leaf', 'raisins'];
+bakeryB = ['red bean', 'dijon mustard', 'apples'];
+recipes = [
+  {
+    name: 'Potato Ganache',
+    ingredients: ['potatoes', 'chocolate']
+  },
+  {
+    name: 'Sweet Fish',
+    ingredients: ['anchovies', 'honey']
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ['dijon mustard', 'raisins']
+  }
+];
+
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
