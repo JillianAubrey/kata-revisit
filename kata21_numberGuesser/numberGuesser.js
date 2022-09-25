d37be2e38dd32c9675f4a12aac5278795fa45ec9
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-25 20:22:30 +0000

    Created numberGuesser function and had it generate an answer

--- kata21_numberGuesser/numberGuesser.js
+++ kata21_numberGuesser/numberGuesser.js
@@ -1,11 +1,8 @@
 let prompt = require("prompt-sync")();
 
-console.log("Welcome to number guesser!");
-console.log("Enter stop at any time to exit");
-
 const screenInputForStop = function(userPrompt = '> ') {
-  const input = prompt(userPrompt).toLowerCase().trim();
-  if (input === 'stop') {
+  const input = prompt(userPrompt).toLowerCase();
+  if (input.search('stop') > -1) {
     console.log('Game stopped');
     process.exit()
   }
@@ -19,6 +16,18 @@ const getMaxNumber = function (){
     console.log("It must be a positive integer greater than 1.")
     maxNum = Number(screenInputForStop());
   } while (maxNum <= 1 || !Number.isInteger(maxNum));
+  return maxNum;
 }
 
-getMaxNumber();
+const numberGuesser = function() {
+  const maxNum = getMaxNumber();
+  let input;
+  let answer = Math.ceil(Math.random() * maxNum);
+
+  console.log('answer:', answer);
+}
+
+
+console.log("Welcome to number guesser!");
+console.log("Enter stop at any time to exit");
+numberGuesser();
